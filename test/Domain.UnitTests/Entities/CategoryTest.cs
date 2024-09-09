fd9ed02634b32356705da7c91804ac6a102cb0e8
kauatwn <kaualopescontato@gmail.com>
2024-09-09 20:50:01 -0300

    Criando teste unitário para Category

---
+++ test/Domain.UnitTests/Entities/CategoryTest.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+
+namespace Domain.UnitTests.Entities;
+
+public class CategoryTest
+{
+    [Fact]
+    public void ShouldCreateCategoryAndSetProperties()
+    {
+        // Arrange & Act
+        var category = new Category(1, "Electronics");
+
+        // Assert
+        Assert.NotNull(category);
+        Assert.Equal(1, category.Id);
+        Assert.Equal("Electronics", category.Name);
+    }
+}
