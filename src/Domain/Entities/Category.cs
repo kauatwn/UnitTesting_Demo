9cb8cae684a13f4a5f0e29da10b67717d5e94ca7
kauatwn <kaualopescontato@gmail.com>
2024-09-10 15:03:07 -0300

    Adicionando método UpdateName à classe Category

--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -4,4 +4,14 @@ public class Category(int id, string name)
 {
     public int Id { get; private set; } = id;
     public string Name { get; private set; } = name;
+
+    public void UpdateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be empty or whitespace.");
+        }
+
+        Name = name;
+    }
 }
